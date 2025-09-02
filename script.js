2cbff3c16d49fa0a03823eeef85de6f4f1496595
shreeharini77 <shreeharini0707@gmail.com>
2025-09-02 20:40:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scrolling for navigation
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
